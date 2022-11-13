ec6e2cc2eb57076775518f068d7358021b7a1445
Igor Shayderov <flashmob123@gmail.com>
2022-11-14 00:46:05 +0200

    Authentication form style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/AuthenticationForm.vue';
 
 export default defineComponent({
   name: 'App',
@@ -16,12 +15,33 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+html {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+
+  --background-color: #041325;
+  --sub-background-color: #232e3a;
+  --border-color: #d6dbfc;
+  --text-color: #ffffff;
+  --success-color: #2e7421;
+  --alert-color: #f20b0b;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  background-color: var(--background-color);
+  color: var(--text-color);
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 1rem;
+  height: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AuthenticationForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <div :class="$style['form-wrapper']">
+    <h1 :class="$style.title">Authentification form</h1>
+
+    <form :class="$style.form" action="">
+      <label for="email" :class="$style.label">
+        Email:
+        <input
+          type="email"
+          name="email"
+          id="email"
+          :class="$style.input" />
+      </label>
+
+      <label for="password" :class="$style.label">
+        Password:
+        <input
+          type="password"
+          name="password"
+          id="password"
+          :class="$style.input" />
+      </label>
+
+      <button type="submit">Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style lang="scss" module>
+.form-wrapper {
+  background-color: var(--sub-background-color);
+  border-radius: 5px;
+  border: 1px solid var(--border-color);
+  margin: auto;
+  padding: 15px;
+  width: 350px;
+}
+
+.title {
+  margin: 0 0 15px;
+}
+
+.form {
+
+}
+
+.label {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 0 10px;
+}
+
+.input {
+  box-sizing: border-box;
+  font-size: 1rem;
+  height: 32px;
+  width: 100%;
+}
+
+.submit-btn {
+  background-color: var(--success-color);
+  border: none;
+  border-radius: 5px;
+  color: var(--text-color);
+  font-size: 1rem;
+  font-weight: 600;
+  margin: 10px 0 0;
+  padding: 5px 10px;
+  width: 100%;
+}
+
+.submit-btn_disabled {
+  opacity: 0.6;
+}
+</style>
